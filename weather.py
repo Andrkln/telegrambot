3244e9ac424bba7e63a1e0f1caf02da1cc2d3035
Andrkln <andrew13338@gmail.com>
2023-10-08 18:35:41 +0300

    requirements.txt added

--- weather.py
+++ weather.py
@@ -1,6 +1,7 @@
 import requests, json
+from decouple import config
 
-waipi = "982f6a34a97a2023cb183fafabb2407e"
+waipi = config('WAIPI')
 
 def get_weather(city):
     r = requests.get(f'http://api.openweathermap.org/data/2.5/weather?appid={waipi}&q={city}&units=metric')
