9bcfd15e7943960fb085f33d9b165d24ff285c76
Andrkln <andrew13338@gmail.com>
2023-12-04 15:06:27 +0200

    all works1

--- spendings.py
+++ spendings.py
@@ -41,17 +41,19 @@ def display_spending_summary(message):
     spendings_by_category = {}
     total_spent = 0
 
+
     for category, price, date, event in rows:
         total_spent += price
         if category not in spendings_by_category:
             spendings_by_category[category] = []
-            spendings_by_category[category].append((price, date, event))
+        spendings_by_category[category].append((price, date, event))
 
     message_parts = [f"You spent {total_spent}. Your spendings:"]
     for category, spendings in spendings_by_category.items():
         category_total = sum([price for price, _, _ in spendings])
-        category_spendings = ', '.join([f"{price} on {event} at {date.strftime('%d.%m %H:%M')}" for price, date, event in spendings])
+        category_spendings = ', '.join([f"{price} on {event} at {date.strftime('%d.%m %H:%M')}\n" for price, date, event in spendings])
         message_parts.append(f"{category} {category_total}: {category_spendings}")
+
 
 
     final_message = "\n\n".join(message_parts)
